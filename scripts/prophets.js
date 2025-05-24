48b1da6861f68efc6c6669b7c4e68d99d5e145d4
carlozserra <cserra@byupathway.edu>
2025-05-24 16:01:58 -0300

    prophets

---
+++ scripts/prophets.js
@@ -0,0 +1,43 @@
+
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        //HTML
+
+        let card = document.createElement('section');
+        let fullName = document.createElement('h1');
+        let portrait = document.createElement('img');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+
+        // Build the h2 content out to show the prophet's full name
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthDate.textContent = `Birthdate: ${prophet.birthdate}`;
+        birthPlace.textContent = `BirthPlace: ${prophet.birthplace}`;
+
+        // Build the image portrait
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
+
+async function getProphetData() {
+    const response = await fetch("https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json");
+    const data = await response.json(); //turn data into JSON
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+
+}
+
+getProphetData();
+
